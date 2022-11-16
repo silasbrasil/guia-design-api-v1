83f6cd2e8c45a94407144bf6fcc86bf74e3b507e
gupy <gupy@MacBook-Pro-de-gupy.local>
2022-11-16 16:36:55 -0300

    First commit

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,21 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsUrl, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+	@IsNotEmpty()
+	@MinLength(1)
+	firstName: string;
+
+	@IsOptional()
+	@MinLength(1)
+	lastName: string;
+
+	@IsEmail()
+	email: string;
+
+	@IsOptional()
+	gender: string;
+
+	@IsUrl()
+	@IsOptional()
+	avatar: string;
+}
