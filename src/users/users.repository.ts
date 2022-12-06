a809e7adc4cf79494ea42d52dead8104de72cc65
gupy <gupy@MacBook-Pro-de-gupy.local>
2022-12-06 14:54:20 -0300

    Implemented users.repository and remove users.repository.spec

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,28 +1,55 @@
-import { Injectable } from "@nestjs/common";
-import { User } from "./entities/user.entity";
+import { Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
+import { USERS_MOCK } from '../__mocks__/USERS_MOCK';
 
 
 @Injectable()
 export class UsersRepository {
 
   create(user: Omit<User, 'id'>) {
-    return user;
+    const id = 'uuid';
+    return USERS_MOCK.push({
+      ...user,
+      id
+    });
   }
 
   findById(id: string) {
-    return {};
+    return USERS_MOCK
+      .find((user) => user.id === id);
   }
 
   // Must be paginated
   findAll() {
-    return [];
+    return USERS_MOCK;
   }
 
   update(id: string, updateUser: Partial<User>) {
-    return {};
+    const user = USERS_MOCK
+      .find((user) => user.id === id);
+
+    if (user) {
+      user.firstName = updateUser.firstName ?? user.firstName;
+      user.lastName = updateUser.lastName ?? user.lastName;
+      user.email = updateUser.email ?? user.email;
+      user.gender = updateUser.gender ?? user.gender;
+      user.avatar = updateUser.avatar ?? user.avatar;
+    } else {
+      throw new Error(`User with id ${id} not found`);
+    }
+
+    return user;
   }
 
   delete(id: string) {
+    const index = USERS_MOCK.findIndex((user) => user.id === id);
 
+    if (index === -1) {
+      throw new Error(`User with id ${id} not found`);
+    }
+
+    USERS_MOCK.splice(index, 1);
+
+    return id;
   }
 }
