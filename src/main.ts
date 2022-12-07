209d2ce28f656bce38d2d9102b13063ec011735f
gupy <gupy@MacBook-Pro-de-gupy.local>
2022-12-07 18:15:28 -0300

    Set version, validationPipe whiteList, cors

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const validationPipe = new ValidationPipe({
+    whitelist: true
+  });
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
+  app
+    .setGlobalPrefix('v1')
+    .useGlobalPipes(validationPipe)
+    .enableCors();
 
   await app.listen(3000);
 }
