bc5c30d5ae864b64b2633be21b5845c0fb50379b
gupy <gupy@MacBook-Pro-de-gupy.local>
2022-12-07 18:49:43 -0300

    Setup throttlerModule do app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ThrottlerModule.forRoot({
+      ttl: 1,
+      limit: 20,
+    }),
+    UsersModule
+  ],
 })
 export class AppModule {}
